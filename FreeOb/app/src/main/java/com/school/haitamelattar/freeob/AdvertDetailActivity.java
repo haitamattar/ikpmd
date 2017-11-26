ae519cf0f6c5bd49c04e96f59535d0718d75ad5e
Haitam el Attar <haitam@live.nl>
2017-11-26 17:54:35 +0100

    adverts visitors android

--- FreeOb/app/src/main/java/com/school/haitamelattar/freeob/AdvertDetailActivity.java
+++ FreeOb/app/src/main/java/com/school/haitamelattar/freeob/AdvertDetailActivity.java
@@ -1,24 +1,42 @@
 package com.school.haitamelattar.freeob;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 import com.school.haitamelattar.freeob.model.Advert;
+import com.school.haitamelattar.freeob.model.User;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class AdvertDetailActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
     private NavigationView navigationView;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +68,13 @@ public class AdvertDetailActivity extends AppCompatActivity {
         categoryTextView.setText(advert.getCategoryName());
         titleTextView.setText(advert.getName());
         descriptionTextView.setText(advert.getDescription());
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        // User details
+        SharedPreferences settings = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+        String userid = settings.getString("id", "");
+
+        visitAdvert(userid, advert.getId(),requestQueue);
     }
 
     public void expandText(View view) {
@@ -100,6 +125,39 @@ public class AdvertDetailActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    // Visit advert
+    public void visitAdvert(String userId, String advertId, RequestQueue requestQueue) {
+        String url = "http://192.168.1.36:8888/visitAdvert";
+        // Post params to be sent to the server
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("userId", userId);
+        params.put("advertId", advertId);
+
+        JsonObjectRequest request_json = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d("Count advert", response.toString());
+                } catch (Exception e) {
+                    Log.d("Fout geen visitor maken", e + "Std");
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+//            success.setMessage("Wrong email or password");
+//            success.setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error: \n\n", error.toString());
+            }
+        });
+
+        requestQueue.add(request_json);
+
     }
 
 
--- FreeOb/app/src/main/java/com/school/haitamelattar/freeob/SettingActivity.java
+++ FreeOb/app/src/main/java/com/school/haitamelattar/freeob/SettingActivity.java
@@ -12,9 +12,32 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Comment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import static android.R.attr.name;
 
@@ -23,6 +46,12 @@ public class SettingActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
     private NavigationView navigationView;
+    private RequestQueue requestQueue;
+    ArrayList<Entry> yvalues = new ArrayList<Entry>();
+    ArrayList<String> xVals = new ArrayList<String>();
+    PieChart pieChart;
+
+
 
     Button logoutBtn;
     @Override
@@ -57,6 +86,89 @@ public class SettingActivity extends AppCompatActivity {
                 startActivity(home);
             }
         });
+        pieChart = (PieChart) findViewById(R.id.piechart);
+
+        pieChart.setUsePercentValues(false);
+
+//        yvalues.add(new Entry(8f, 0));
+//        yvalues.add(new Entry(15f, 1));
+//        yvalues.add(new Entry(12f, 2));
+//        yvalues.add(new Entry(25f, 3));
+//        yvalues.add(new Entry(23f, 4));
+//        yvalues.add(new Entry(17f, 5));
+
+//        PieDataSet dataSet = new PieDataSet(yvalues, "Adverts");
+
+
+
+//        xVals.add("January");
+//        xVals.add("February");
+//        xVals.add("March");
+//        xVals.add("April");
+//        xVals.add("May");
+//        xVals.add("June");
+
+//        PieData data = new PieData(xVals, dataSet);
+
+//        pieChart.setData(data);
+//        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        pieChart.setDescription("Visitors of every published advert");
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        // User details
+        SharedPreferences settings = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+        String userid = settings.getString("id", "");
+
+        visitAdvertCount(userid, requestQueue);
+    }
+
+    // Visit advert
+    public void visitAdvertCount(String userId, RequestQueue requestQueue) {
+        String url = "http://school.haitamattar.com/advertUserVisitors?userId="+userId;
+        Log.d("userid", userId);
+        JsonArrayRequest request_json = new JsonArrayRequest(Request.Method.GET, url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    Log.d("Count advert VISITORS", response.toString());
+
+                    for(int i = 0; i < response.length(); i++){
+                        JSONObject jsonObj = response.getJSONObject(i);
+                        String advertName = jsonObj.getString("advert_name");
+                        int advertVisitCount = jsonObj.getInt("visitors");
+
+                        // Data to be used in a pieChart
+                        // Log.d("Advert name: ", advertName + " Visitors: " + advertVisitCount);
+                        yvalues.add(new Entry(advertVisitCount, i));
+                        xVals.add(advertName);
+                    }
+
+                    // Init pie with data
+                    PieDataSet dataSet = new PieDataSet(yvalues, "Adverts");
+                    PieData data = new PieData(xVals, dataSet);
+                    // Convert float to int
+                    data.setValueFormatter(new ValueFormatter());
+                    // Set data
+                    pieChart.setData(data);
+                    dataSet.setValueTextSize(10);
+                    pieChart.setDrawHoleEnabled(false);
+                    dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+                } catch (Exception e) {
+                    Log.d("Fout geen visitor", e + "Std");
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                pieChart.setEnabled(false);
+                Log.d("Wrong Error: \n\n", error.toString());
+            }
+        });
+
+        requestQueue.add(request_json);
 
     }
 
@@ -66,6 +178,7 @@ public class SettingActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 
     /**
      * Handles the navigation menu Items on SettingActivity
@@ -101,5 +214,14 @@ public class SettingActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+}
+
+// Percent (float) to int
+class ValueFormatter implements com.github.mikephil.charting.formatter.ValueFormatter{
+
+    @Override
+    public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+        return "" + ((int) value);
     }
 }
